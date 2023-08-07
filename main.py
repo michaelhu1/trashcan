82fd120943f8cf8c959046cb5c3aeec1caf8be0e
michaelhu1 <michaelhu1@users.noreply.github.com>
2023-08-07 21:21:34 +0800

    update detect to print class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from detect import *
+import json
 
 model = model_setup()
 camera = camera_setup()
@@ -6,5 +7,8 @@ camera = camera_setup()
 clear_image()
 
 result =  classify(model,camera)
-print(result)
+result_dict = json.loads(result)
+prediction = result_dict["predictions"][0]
+print(prediction["class"])
+
 clear_image()
