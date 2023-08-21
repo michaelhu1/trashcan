91c54af816d575eea636feff1219d9bfec9e8744
michaelhu1 <michaelhu1@users.noreply.github.com>
2023-08-21 21:28:43 +0800

    Update display.py

--- display.py
+++ display.py
@@ -1,6 +1,5 @@
 from guizero import *
-
-app = App(title = "Recycling Bin", height = 480, width = 800)
+from tkinter import pack
 
 def update_writing():
     writing.set("World")
@@ -8,10 +7,8 @@ def update_writing():
 
 app = App(height=480, width=800, bg="black")
 
-writing = Text(app, text="One piece of trash at a time", color="red", size=110)
-
+writing = Text(app, text="One piece of trash at a time", color="white", size=40)
 writing.pack(fill="none", expand=True)
-
 app.after(200, update_writing)
 
 app.display()
