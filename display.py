117ab71028a20c22f4d0ec1dedca197b2444406d
michaelhu1 <michaelhu1@users.noreply.github.com>
2023-08-21 20:52:59 +0800

    Update display.py

--- display.py
+++ display.py
@@ -1,5 +1,5 @@
 from guizero import *
 
-app = App(title = "Recycling Bin", height = 480, width = 800)
-start_message = Text(app, text = "Please put one piece of trash at a time", size = 40)
+app = App(title = "Recycling Bin", height = 480, width = 800, layout = "grid")
+start_message = Text(app, text = "One piece of trash at a time", size = 40, grid = [400,240])
 app.display()
