e1b1f0dddc627e5dc63c60a025fe0ed982a7dd4a
michaelhu1 <michaelhu1@icloud.com>
2023-08-22 12:53:54 +0800

    Update displaytest.py

--- displaytest.py
+++ displaytest.py
@@ -1,10 +1,11 @@
 import tkinter
 import customtkinter
 
-customtkinter.set_appearance_mode("system")  
+app = customtkinter.CTk()
+
+
 customtkinter.set_default_color_theme("blue")
 
-app = customtkinter.CTk()
 app.geometry("800x480")
 app.title("Trash Can")
 
